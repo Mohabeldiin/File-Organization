600c29594278c4ac2456b71c814df05721c3c637
Mohab Mohsen <mohabeldiin@gmail.com>
2021-05-28 04:02:54 +0200

    init Class for recoreds

---
+++ Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Organization
+{
+    class Class
+    {
+        public static String FileName = "";
+        public static int Recor_Size = 32;
+        public static int Counter = 0;
+    }
+}
