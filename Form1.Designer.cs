ed4506a43a0ea47a0d34ad000dd3a6fbd4e435b3
Mohab Mohsen <mohabeldiin@gmail.com>
2021-05-28 02:49:54 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace File_Organization
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Create = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.TheLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.TextBox();
+            this.Gender = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(252, 40);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(94, 29);
+            this.Create.TabIndex = 2;
+            this.Create.Text = "Create";
+            this.Create.UseVisualStyleBackColor = true;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(352, 40);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(94, 29);
+            this.Open.TabIndex = 3;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(452, 40);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(94, 29);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // TheLabel
+            // 
+            this.TheLabel.AutoSize = true;
+            this.TheLabel.Location = new System.Drawing.Point(139, 18);
+            this.TheLabel.Name = "TheLabel";
+            this.TheLabel.Size = new System.Drawing.Size(50, 20);
+            this.TheLabel.TabIndex = 5;
+            this.TheLabel.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Year";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Gender";
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(109, 97);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(125, 27);
+            this.ID.TabIndex = 11;
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(109, 130);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(125, 27);
+            this.Name.TabIndex = 12;
+            // 
+            // Phone
+            // 
+            this.Phone.Location = new System.Drawing.Point(109, 163);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(125, 27);
+            this.Phone.TabIndex = 13;
+            // 
+            // Year
+            // 
+            this.Year.Location = new System.Drawing.Point(109, 196);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(125, 27);
+            this.Year.TabIndex = 14;
+            // 
+            // Gender
+            // 
+            this.Gender.Location = new System.Drawing.Point(109, 229);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(125, 27);
+            this.Gender.TabIndex = 15;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(127, 273);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(94, 29);
+            this.Insert.TabIndex = 16;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(272, 273);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(106, 29);
+            this.Search.TabIndex = 17;
+            this.Search.Text = "Search By ID";
+            this.Search.UseVisualStyleBackColor = true;
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(404, 273);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(106, 29);
+            this.Display.TabIndex = 18;
+            this.Display.Text = "Display All";
+            this.Display.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(252, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(294, 159);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 540);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TheLabel);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Label TheLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Phone;
+        private System.Windows.Forms.TextBox Year;
+        private System.Windows.Forms.TextBox Gender;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Display;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
